5c53346e9840cfb499c1dda98da447656c89bee3
ymgn9314 <yamagen.private@gmail.com>
2022-05-04 14:19:38 +0900

    symlink修正

--- symlink/zsh/.zsh/alias.zsh
+++ symlink/zsh/.zsh/alias.zsh
@@ -1,3 +1,11 @@
-# aliases
+# fvm
 alias f="fvm flutter"
 alias d="fvm dart"
+
+# git
+alias g="git"
+alias gs="git status"
+alias gd="git diff"
+alias gdc="git diff --cached"
+alias gpl="git pull"
+alias gps="git push"
