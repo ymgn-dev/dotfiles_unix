63be05ea6df08fdc4b351ad3b551c578b8d49610
ymgn9314 <yamagen.private@gmail.com>
2022-05-11 16:40:07 +0900

    add plugins

--- symlink/nvim/.config/nvim/lua/plugins.lua
+++ symlink/nvim/.config/nvim/lua/plugins.lua
@@ -23,11 +23,32 @@ local function init()
     'nvim-lualine/lualine.nvim',
     config = function()
       require('lualine').setup()
-    end,
+    end
   }
   use {
     'nvim-telescope/telescope.nvim',
     requires = { 'nvim-lua/plenary.nvim' }
+  }
+  use {
+    'kyazdani42/nvim-tree.lua',
+    requires = {
+      'kyazdani42/nvim-web-devicons',
+    },
+    tag = 'nightly',
+    config = function()
+      require('nvim-tree').setup{}
+    end
+  }
+  use {
+    'akinsho/bufferline.nvim',
+    tag = "v2.*",
+    requires = {
+      'kyazdani42/nvim-web-devicons'
+    },
+    config = function()
+      vim.opt.termguicolors = true
+      require("bufferline").setup{}
+    end
   }
 end
 
