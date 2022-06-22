ba47cf3af1b06fc1bb37245803beab62dc407afd
ymgn9314 <yamagen.private@gmail.com>
2022-06-22 17:45:33 +0900

    update

--- symlink/zsh/.zsh/env.zsh
+++ symlink/zsh/.zsh/env.zsh
@@ -5,3 +5,7 @@ export PATH="$PATH":"$HOME/.pub-cache/bin":"$HOME/fvm/default/bin":"/opt/homebre
 # コンパイラにopenjdkの場所を教える設定
 # export CPPFLAGS="-I/opt/homebrew/opt/openjdk/include"
 export CPPFLAGS="-I/opt/homebrew/opt/openjdk@11/include"
+
+export LANG="en_US.UTF-8"
+export LC_ALL="en_US.UTF-8"
+export LC_CTYPE="en_US.UTF-8"
